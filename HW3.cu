cc7f3901b2b802d6129304f9c511037e201fd3c5
JaDarrienTatum <123326772+JaDarrienTatum@users.noreply.github.com>
2023-01-30 13:02:40 -0600

    Update HW3.cu

--- HW3.cu
+++ HW3.cu
@@ -62,7 +62,7 @@ void CleanUp()
 //It adds vectors A and B then stores result in vector C
 __global__ void AdditionGPU(float *a, float *b, float *c, int n)
 {
-int id = threadIdx.x
+int id = threadIdx.x;
 	
 	if(id < N)
 	{
