204adaadc9a431385559b05ea8b8fca221df08bc
JaDarrienTatum <123326772+JaDarrienTatum@users.noreply.github.com>
2023-02-06 14:28:29 -0600

    Create HW5.cu

---
+++ HW5.cu
@@ -0,0 +1,118 @@
+// large Vector addition on the GPU with a fixed number of blocks.
+// nvcc HW5.cu -o LargeVectorAdditionGPUFixedBlocksAndGrid
+
+#include <sys/time.h>
+#include <stdio.h>
+
+//Length of vectors to be added.
+#define N 217 // ** Don't change this
+
+//Globals
+float *A_CPU, *B_CPU, *C_CPU; //CPU pointers
+float *A_GPU, *B_GPU, *C_GPU; //GPU pointers
+dim3 BlockSize; //This variable will hold the Dimensions of your block
+dim3 GridSize; //This variable will hold the Dimensions of your grid
+
+//This will be the layout of the parallel space we will be using.
+void SetUpCudaDevices()
+{
+	BlockSize.x = 4; // ** Don't change this
+	BlockSize.y = 1;
+	BlockSize.z = 1;
+	
+	GridSize.x = 5; // ** Don't change this
+	GridSize.y = 1;
+	GridSize.z = 1;
+}
+
+//Sets a side memory on the GPU and CPU for our use.
+void AllocateMemory()
+{					
+	//Allocate Device (GPU) Memory
+	cudaMalloc(&A_GPU,N*sizeof(float));
+	cudaMalloc(&B_GPU,N*sizeof(float));
+	cudaMalloc(&C_GPU,N*sizeof(float));
+
+	//Allocate Host (CPU) Memory
+	A_CPU = (float*)malloc(N*sizeof(float));
+	B_CPU = (float*)malloc(N*sizeof(float));
+	C_CPU = (float*)malloc(N*sizeof(float));
+}
+
+//Loads values into vectors that we will add.
+void Innitialize()
+{
+	int i;
+	
+	for(i = 0; i < N; i++)
+	{		
+		A_CPU[i] = (float)2*i;	
+		B_CPU[i] = (float)i;
+	}
+}
+
+//Cleaning up memory after we are finished.
+void CleanUp()
+{
+	free(A_CPU); free(B_CPU); free(C_CPU);
+	cudaFree(A_GPU); cudaFree(B_GPU); cudaFree(C_GPU);
+}
+
+//This is the kernel. It is the function that will run on the GPU.
+//It adds vectors A and B then stores result in vector C
+__global__ void AdditionGPU(float *a, float *b, float *c, int n)
+{
+	???
+}
+
+int main()
+{
+	int i;
+	timeval start, end;
+	
+	//Set the thread structure that you will be using on the GPU	
+	SetUpCudaDevices();
+
+	//Partitioning off the memory that you will be using.
+	AllocateMemory();
+
+	//Loading up values to be added.
+	Innitialize();
+	
+	//Starting the timer
+	gettimeofday(&start, NULL);
+
+	//Copy Memory from CPU to GPU		
+	cudaMemcpyAsync(A_GPU, A_CPU, N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpyAsync(B_GPU, B_CPU, N*sizeof(float), cudaMemcpyHostToDevice);
+	
+	//Calling the Kernel (GPU) function.	
+	AdditionGPU<<<GridSize,BlockSize>>>(A_GPU, B_GPU, C_GPU, N);
+	
+	//Copy Memory from GPU to CPU	
+	cudaMemcpyAsync(C_CPU, C_GPU, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+	//Stopping the timer
+	gettimeofday(&end, NULL);
+
+	//Calculating the total time used in the addition and converting it to milliseconds.
+	float time = (end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec);
+	
+	//Displaying the time 
+	printf("Time in milliseconds= %.15f\n", (time/1000.0));	
+
+	// Displaying the vector. You will want to comment this out when the vector gets big.
+	// This is just to make sure everything is running correctly.	
+	for(i = 0; i < N; i++)		
+	{		
+		printf("A[%d] = %.15f  B[%d] = %.15f  C[%d] = %.15f\n", i, A_CPU[i], i, B_CPU[i], i, C_CPU[i]);
+	}
+
+	//Displaying the last value of the addition for a check when all vector display has been commented out.
+	printf("Last Values are A[%d] = %.15f  B[%d] = %.15f  C[%d] = %.15f\n", N-1, A_CPU[N-1], N-1, B_CPU[N-1], N-1, C_CPU[N-1]);
+	
+	//You're done so cleanup your mess.
+	CleanUp();	
+	
+	return(0);
+}
