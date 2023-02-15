f8de71424d9c29d90537454f2985189083b8bb6e
JaDarrienTatum <123326772+JaDarrienTatum@users.noreply.github.com>
2023-02-15 14:31:59 -0600

    Create MyCuda.h

---
+++ MyCuda.h
@@ -0,0 +1,11 @@
+void myCudaErrorCheck(const char* file, int line)
+{
+	cudaError_t error;
+	error = cudaGetLastError();
+
+	if (error != cudaSuccess)
+	{
+		printf("\n CUDA message = %s, File = %s, Line = %d\n", cudaGetErrorString(error), file, line);
+		exit(0);
+	}
+}
