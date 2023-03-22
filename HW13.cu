f74b39fc5930a0a0ee6913ff5282a8361c60edc2
JaDarrienTatum <123326772+JaDarrienTatum@users.noreply.github.com>
2023-03-22 12:05:56 -0500

    Create HW13.cu

---
+++ HW13.cu
@@ -0,0 +1,157 @@
+// nvcc HW13.cu -o temp
+
+#include <sys/time.h>
+#include <stdio.h>
+#include "./MyCuda.h"
+
+#define SIZE 2000000 
+#define NUMBER_OF_COPIES 1000
+
+//Function prototypes
+void setUpCudaDevices();
+void allocateMemory();
+void cleanUp();
+void copyPageableMemoryUp();
+void copyPageLockedMemoryUp();
+void copyPageableMemoryDown();
+void copyPageLockedMemoryDown();
+
+//Globals
+float *NumbersOnGPU, *PageableNumbersOnCPU, *PageLockedNumbersOnCPU;
+cudaEvent_t StartEvent, StopEvent;
+
+//This will be the layout of the parallel space we will be using.
+void setUpCudaDevices()
+{
+	cudaEventCreate(&StartEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventCreate(&StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+}
+
+//Sets a side memory on the GPU and CPU for our use.
+void allocateMemory()
+{					
+	//Allocate Device (GPU) Memory
+	cudaMalloc(&NumbersOnGPU, SIZE*sizeof(float));
+	myCudaErrorCheck(__FILE__, __LINE__);
+
+	//Allocate pageable Host (CPU) Memory
+	PageableNumbersOnCPU = (float*)malloc(SIZE*sizeof(float));
+	
+	//Allocate page locked Host (CPU) Memory
+	?????
+	myCudaErrorCheck(__FILE__, __LINE__);
+}
+
+//Cleaning up memory after we are finished.
+void cleanUp()
+{
+	cudaFree(NumbersOnGPU); 
+	myCudaErrorCheck(__FILE__, __LINE__);
+	
+	?????
+	myCudaErrorCheck(__FILE__, __LINE__);
+	
+	free(PageableNumbersOnCPU); 
+	
+	cudaEventDestroy(StartEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventDestroy(StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+}
+
+void copyPageableMemoryUp()
+{
+	for(int i = 0; i < NUMBER_OF_COPIES; i++)
+	{
+		cudaMemcpy(NumbersOnGPU, PageableNumbersOnCPU, SIZE*sizeof(float), cudaMemcpyHostToDevice);
+		myCudaErrorCheck(__FILE__, __LINE__);
+	}
+}
+
+void copyPageableMemoryDown()
+{
+	for(int i = 0; i < NUMBER_OF_COPIES; i++)
+	{
+		cudaMemcpy(PageableNumbersOnCPU, NumbersOnGPU, SIZE*sizeof(float), cudaMemcpyDeviceToHost);
+		myCudaErrorCheck(__FILE__, __LINE__);
+	}
+}
+
+void copyPageLockedMemoryUp()
+{
+	for(int i = 0; i < NUMBER_OF_COPIES; i++)
+	{
+		?????
+		myCudaErrorCheck(__FILE__, __LINE__);
+	}
+}
+
+void copyPageLockedMemoryDown()
+{
+	for(int i = 0; i < NUMBER_OF_COPIES; i++)
+	{
+		?????
+		myCudaErrorCheck(__FILE__, __LINE__);
+	}
+}
+
+
+int main()
+{
+	float timeEvent;
+	
+	setUpCudaDevices();
+	allocateMemory();
+	
+	cudaEventRecord(StartEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	copyPageableMemoryUp();
+	cudaEventRecord(StopEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventSynchronize(StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventElapsedTime(&timeEvent, StartEvent, StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	printf("\n Time on GPU using pageable memory up = %3.1f milliseconds", timeEvent);
+	
+	cudaEventRecord(StartEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	copyPageLockedMemoryUp();
+	cudaEventRecord(StopEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventSynchronize(StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventElapsedTime(&timeEvent, StartEvent, StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	printf("\n Time on GPU using page locked memory up = %3.1f milliseconds", timeEvent);
+	
+	cudaEventRecord(StartEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	copyPageableMemoryDown();
+	cudaEventRecord(StopEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventSynchronize(StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventElapsedTime(&timeEvent, StartEvent, StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	printf("\n Time on GPU using pageable memory down = %3.1f milliseconds", timeEvent);
+	
+	cudaEventRecord(StartEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	copyPageLockedMemoryDown();
+	cudaEventRecord(StopEvent, 0);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventSynchronize(StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	cudaEventElapsedTime(&timeEvent, StartEvent, StopEvent);
+	myCudaErrorCheck(__FILE__, __LINE__);
+	printf("\n Time on GPU using page locked memory down = %3.1f milliseconds", timeEvent);
+	
+	printf("\n");
+	//You're done so cleanup your mess.
+	cleanUp();	
+	
+	return(0);
+}
